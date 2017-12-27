f1822588e635e7da7961ddb4542124c67f802a46
Shehanka <hcsperera@outlook.com>
2017-12-27 21:54:46 +0530

    Initial commit

---
+++ loginController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chamodshehanka
+ * Date: 12/24/2017
+ * Time: 9:45 PM
+ */
+
+$userName = $_POST["txtUserName"];
+$password = $_POST["txtPassword"];
+
+$connection = mysqli_connect("localhost","root","wampwamp","dominos_web");
+
+if ($connection){
+    $SQL = "SELECT * FROM user";
+    $result = mysqli_query($connection,$SQL);
+    if ($result){
+        $row = mysqli_fetch_row($result);
+        if ($userName === $row[0] && $password === $row[1]){
+            $type = $row[2];
+            if ($type === 'admin') {
+                header("Location: admin.html");
+            }else if ($type === 'chef'){
+                header("Location: http://www.google.com");
+            }
+        }
+    }
+}else{
+    echo 'something went wrong check connection';
+}
